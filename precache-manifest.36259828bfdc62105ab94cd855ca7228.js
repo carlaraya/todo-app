6913f7707f8eeb53d40d1baf3ecd234395e717ce
carlaraya <carl.araya256@gmail.com>
2019-02-25 18:04:38 +0800

    Updates

---
+++ precache-manifest.36259828bfdc62105ab94cd855ca7228.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4ef54527c02e253fb74d",
+    "url": "/todo-app/static/js/runtime~main.4ef54527.js"
+  },
+  {
+    "revision": "44573030ce79855506df",
+    "url": "/todo-app/static/js/main.44573030.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/todo-app/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "44573030ce79855506df",
+    "url": "/todo-app/static/css/main.25e547b3.chunk.css"
+  },
+  {
+    "revision": "634fc0dad4406777fa341b141eb9c56e",
+    "url": "/todo-app/index.html"
+  }
+];
